5d3dc9d8c411861df1eade4c5a47a5cca87cbabc
Michael Dougherty <maackle.d@gmail.com>
2019-07-23 15:56:34 -0700

    remove t.plan

--- test/unit_test/host_test.js
+++ test/unit_test/host_test.js
@@ -6,12 +6,11 @@ module.exports = (scenario) => {
       host_doc:{
       kyc_proof: "DOC # QuarnnnnvltuenblergjasnvAfs"
     }}
-    t.plan(2)
+
     const verified = await liza.call("host", "register_as_host", Host_Doc);
     console.log("verified:: ",verified);
     t.equal(verified.Ok.length, 46)
 
-    
     const is_verified = await liza.call("host", "is_registered_as_host", {});
     console.log("is verified?:: ",is_verified);
     t.equal(is_verified.Ok.links.length, 1)
