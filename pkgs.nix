be230d76419bca108a5f94a84e29c6d959b67216
Perry Kundert <perry@hardconsulting.com>
2019-08-23 06:01:19 -0600

    Upgrade to latest holoportos, including python for Scenario tests

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holopkgs/archive/25a3c006700acd5d364f693d8978ae8ff933caeb.tar.gz";
-  sha256 = "0cnd9rgpf3picr041ns240dkgrydn6rsdavnj4sibf36m6mm2cy8";
+  url = "https://github.com/Holo-Host/holopkgs/archive/a8d2488a1f65f86663311f650a14aa18cd98d33d.tar.gz";
+  sha256 = "15vv1c2pphq2b0qkzr2wvznrx3xydsp62rb4g98z08w6d3wz0zkw";
 })
