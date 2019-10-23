d312b6e1209cd5cb1774b073559e4e5fc6a6ac35
dymayday <dymayday@gmail.com>
2019-10-23 16:07:40 +0200

    Update sync call

--- test/unit_test/app_flow_test.js
+++ test/unit_test/app_flow_test.js
@@ -1,7 +1,7 @@
 const { one } = require('../config')
 
 module.exports = (scenario) => {
-  scenario('App Flow Test',async (s, t) => {
+  scenario('App Flow Test', async (s, t) => {
     const { liza } = await s.players({liza: one('liza')}, true)
 
     const App_Config_1 = {
@@ -25,7 +25,7 @@ module.exports = (scenario) => {
         kyc_proof: "DOC # QuarnnnnvltuenblergjasnvAfs"
       }
     }
-    const verified = await liza.callSync("app","host", "register_as_host", Host_Doc);
+    const verified = await liza.call("app","host", "register_as_host", Host_Doc);
     console.log("verified:: ",verified);
     t.ok(verified.Ok)
     const Provider_Doc = {
@@ -33,16 +33,16 @@ module.exports = (scenario) => {
         kyc_proof: "DOC # QuarnnnnvltuenblergjasnvAfs"
       }
     }
-    const verified_provider = await liza.callSync("app","provider", "register_as_provider", Provider_Doc);
+    const verified_provider = await liza.call("app","provider", "register_as_provider", Provider_Doc);
     console.log("verified_provider:: ",verified_provider);
     t.ok(verified_provider.Ok)
 
-    const app_address_1 = await liza.callSync("app", "provider", "register_app", App_Config_1);
-    const app_address_2 = await liza.callSync("app","provider",  "register_app", App_Config_2);
+    const app_address_1 = await liza.call("app", "provider", "register_app", App_Config_1);
+    const app_address_2 = await liza.call("app","provider",  "register_app", App_Config_2);
     console.log("APP ADDRESS 1:: ",app_address_1);
     t.ok(app_address_1.Ok);
 
-    const app_enable = await liza.callSync("app","host","enable_app",{app_hash:app_address_1.Ok});
+    const app_enable = await liza.call("app","host","enable_app",{app_hash:app_address_1.Ok});
     await liza.call("app","host","enable_app",{app_hash:app_address_2.Ok});
     t.equal(app_enable.Ok, null)
 
@@ -54,7 +54,7 @@ module.exports = (scenario) => {
     console.log("Agent List:: ",agent_list);
     t.equal(agent_list.Ok.length, 1)
 
-    const disable_app_hash = await liza.callSync("app","host","disable_app",{app_hash:app_address_2.Ok});
+    const disable_app_hash = await liza.call("app","host","disable_app",{app_hash:app_address_2.Ok});
     t.equal(disable_app_hash.Ok, null)
 
     const app_list_after_disable = await liza.call("app","host","get_enabled_app_list",{});
