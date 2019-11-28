e1b66b157da044d9c3648f86b21958220892eb44
Perry Kundert <perry@hardconsulting.com>
2019-11-28 09:09:04 -0700

    Update holo-nixpkgs

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/79ea8bdd7e2f679879dbe2ef364a467a1c7e3823.tar.gz";
-  sha256 = "0xgryx6cgjnqz2zrzrqamvmh0if55ps1r6szwx56z3ahf08bdna5";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/94e551b13dc04702d209d9936652bcc9d767894a.tar.gz";
+  sha256 = "0ssr2wvrhylysghf4x7ap94zr4dq4qzvazprlk2f6rl4vv4whmkx";
 })
