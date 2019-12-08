0f6ce8ac5a2e54afa021dafafe5139d6957a3dd4
Perry Kundert <perry@hardconsulting.com>
2019-12-08 07:08:17 -0800

    Use build-dna in holo-nixpkgs

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/4a7104e3042f54b1cd454cc7677b3e39e28fac37.tar.gz";
-  sha256 = "1hkdbd49h8q9pqww0h8wvmdc0pymkxd6xqz1bv5xdkisx41s4z43";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/8b8da7f30d7940abeb463ca5b5a6fd3df1ad7bae.tar.gz";
+  sha256 = "0wckq3fcgwva9mpa1857d7s118x2np0n11v3wiljmxalny1df023";
 })
