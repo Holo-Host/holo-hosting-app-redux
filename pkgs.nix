a84a8de8955ce32471aa758021841ff02c1f8f99
Perry Kundert <perry@hardconsulting.com>
2019-12-18 13:55:12 -0800

    Update to holochain-rust develop 2019-12-18

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/998bc9e6a8e61bde47485e0ef90b51cec70a3786.tar.gz";
-  sha256 = "127021rzh9dq87a6v118c5n6jrjhhxv4hdvvprra21jmgfw8nk73";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/8455f9413346a02ad8d50f1a210b5ff884dbc5d7.tar.gz";
+  sha256 = "1p10g3ngjhnhs053rs3x2vy2fbryjx31p9afvi50yjxpkkfbl1xw";
 })
