e23cadc68dda3ba05dcb94c263ec5327cdd7381b
Perry Kundert <perry@hardconsulting.com>
2019-12-19 14:37:57 -0800

    Update Cargo.lock to holochain-rust 0.0.41-alpha3

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/88a325b9b03976d5d84872283f737bd76b8cb7ca.tar.gz";
-  sha256 = "1ma17fpdibdl1clfrpsk2jpa2b9kfyq99nxv82vh6v6wzjd4ml6m";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/a1b09bdc64ed0e512a1a693493bbfb47253902f7.tar.gz";
+  sha256 = "0xj2d37h6dn0z4qqbdvk4378vninis7kwidnpnh3696fx9v1jy7f";
 })
