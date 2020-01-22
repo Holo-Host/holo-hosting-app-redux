30b329c1ee0e4354c8ef05b8651144f01797cc17
zo-el <joelulahanna@gmail.com>
2020-01-22 16:22:41 -0600

    updated the dna to build on hc 0.0.42-alpha5

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/fc160c13bd2f88409d09866ea59945b9579dd23a.tar.gz";
-  sha256 = "09wsb9n62rxgr4pqz2z1098xcj8fqwxnq0iv0lhprj15vbkxpgih";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/aa6826666f94ca70c182f653877fed806fff64c6.tar.gz";
+  sha256 = "1lq7rnsyk2dizysir68p1lf6rsj5w4wkvvm0adj2bh1jnz6ywlhz";
 })
