2e1cdcbe15f94a742b84a704550b1c594dd45234
zo-el <joelulahanna@gmail.com>
2020-03-09 09:23:13 -0500

    updated holochain to 44

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/67610e50a8b7e63b9ef5a7891de293d7b39ce24a.tar.gz";
-  sha256 = "0sv8ldv5cqghlnh47s0ha31r64ih8m1lhg8q08gm46im5phcv6jp";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/d80cf06390d81ec3ab5b0e0c1a275aa7bda103f1.tar.gz";
+  sha256 = "0ggylav9i3h7zc3fcwxsihr8aczwywf9imfdnqbc3z3ydly4hnmv";
 })
