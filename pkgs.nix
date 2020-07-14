5372096af62c106f6bf1780eb715ebf87aae630d
zo-el <joelulahanna@gmail.com>
2020-07-14 12:58:58 -0500

    update to v0.0.50-alpha4

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/75cf8c8e269befccc553d516eb335241c3840d83.tar.gz";
-  sha256 = "089n1i53cdjg2vfwb0ry9ywqpq5ckpfgaghl79blckknclqz7hn5";
+  url = "https://github.com/Holo-Host/holo-nixpkgs/archive/9b8742481881e3b0efe0e7ece9b508b988c27bc7.tar.gz";
+  sha256 = "1zf55bl2pdj7n5f37fr9bigb7y6qbbsxrm1xhihm325y1iwvhskz";
 })
